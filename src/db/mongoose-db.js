3ef1be088dce788325c23424fa211fcb643eadbc
projectdec2021 <103400908+projectdec2021@users.noreply.github.com>
2022-05-05 08:43:54 +0530

    Update mongoose-db.js

--- src/db/mongoose-db.js
+++ src/db/mongoose-db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const connectionUrl = process.env.DB_CONNECTION_URL
 const databaseName = process.env.DATABASE_NAME
 
-mongoose.connect(connectionUrl + '/' + databaseName, {}).then(()=>{
+mongoose.connect('mongodb://mongo:27017/appointme', {}).then(()=>{
     console.log("Database Connection Successful!!!")
 }).catch((err)=>{
     console.log("Database Connection Failed!!!", err)
